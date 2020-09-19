000aa8b69de1a4db90e6f177042f1a37ab3e6979
James <70666457+geblerj@users.noreply.github.com>
2020-09-19 19:47:49 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    Fish jonah = new Fish("Ripper Monster");
+    jonah.move();
+    jonah.move(16);
+    jonah.changeName("Bumblecake the sweetie pie");
+  }
+}
